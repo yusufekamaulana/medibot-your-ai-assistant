f34f3a7df56a110e86a023929050961c6d8faa02
Yusuf <yusuf.eka.maulana-2022@ftmm.unair.ac.id>
2025-06-25 01:12:42 +0700

    jsx mda

--- hooks/use-autosize-textarea.ts
+++ hooks/use-autosize-textarea.ts
@@ -1,7 +1,7 @@
 import { useLayoutEffect, useRef } from "react"
 
 interface UseAutosizeTextAreaProps {
-  ref: React.RefObject<HTMLTextAreaElement>
+  ref: React.RefObject<HTMLTextAreaElement | null>
   maxHeight?: number
   borderWidth?: number
   dependencies: React.DependencyList
@@ -16,9 +16,9 @@ export function useAutosizeTextArea({
   const originalHeight = useRef<number | null>(null)
 
   useLayoutEffect(() => {
-    if (!ref.current) return
-
     const currentRef = ref.current
+    if (!currentRef) return
+
     const borderAdjustment = borderWidth * 2
 
     if (originalHeight.current === null) {
@@ -28,12 +28,11 @@ export function useAutosizeTextArea({
     currentRef.style.removeProperty("height")
     const scrollHeight = currentRef.scrollHeight
 
-    // Make sure we don't go over maxHeight
     const clampedToMax = Math.min(scrollHeight, maxHeight)
-    // Make sure we don't go less than the original height
     const clampedToMin = Math.max(clampedToMax, originalHeight.current)
 
     currentRef.style.height = `${clampedToMin + borderAdjustment}px`
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [maxHeight, ref, ...dependencies])
 }
