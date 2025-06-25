123ae52170d0bb2009b28128e56c8bc60d0191e3
Yusuf <yusuf.eka.maulana-2022@ftmm.unair.ac.id>
2025-06-25 07:11:15 +0700

    debug hook user

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -38,7 +38,7 @@ export function NavUser() {
   //   email: "Logged In",
   //   avatar: "M",
   // }
-  
+
   const handleLogout = () => {
     Cookies.remove("token")
     router.push("/login")
@@ -57,12 +57,12 @@ export function NavUser() {
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarFallback className="rounded-lg">
-                  {user.avatar}
+                  {user!.avatar}
                 </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">{user!.name}</span>
+                <span className="truncate text-xs">{user!.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -77,12 +77,12 @@ export function NavUser() {
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarFallback className="rounded-lg">
-                    {user.avatar}
+                    {user!.avatar}
                   </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">{user!.name}</span>
+                  <span className="truncate text-xs">{user!.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
